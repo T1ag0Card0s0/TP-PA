3ca7f59a104e7675dbeec2484435a20ca53d9077
T1ag0Card0s0 <117290011+T1ag0Card0s0@users.noreply.github.com>
2023-04-03 00:15:46 +0100

    add

---
+++ TinyPAc/src/TinyPAcMain.java
@@ -0,0 +1,22 @@
+import pt.isec.pa.tinypac.gameengine.GameEngine;
+import pt.isec.pa.tinypac.gameengine.IGameEngine;
+import pt.isec.pa.tinypac.gameengine.IGameEngineEvolve;
+
+class TestClient implements IGameEngineEvolve{
+    int count = 0;
+
+    @Override
+    public void evolve(IGameEngine gameEngine, long currentTime) {
+        System.out.printf("[%d] %d\n",currentTime,++count);
+        if(count>=20)gameEngine.stop();
+    }
+}
+public class TinyPAcMain {
+    public static void main(String [] args){
+        IGameEngine gameEngine=new GameEngine();
+        TestClient client= new TestClient();
+        gameEngine.registerClient(client);
+        gameEngine.start(500);
+        gameEngine.waitForTheEnd();
+    }
+}
